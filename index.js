be8474ffa32db04b7c3e9e454464a9ac4e93bbb3
rezley.chua <rezley.chua@orangeandbronze.com>
2016-05-20 17:16:57 +0800

    changed the implementation for extending the lifecycle callback methods.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
+
 var HistoryModelService = require('./lib/HistoryModelService');
 
 module.exports = function (sails) {
   var loader = require('sails-util-mvcsloader')(sails);
-  
 
   loader.configure();
-  return {    
+  return {
     initialize: function (next) {
       loader.inject(function (err) {
         HistoryModelService.createModel(sails);
